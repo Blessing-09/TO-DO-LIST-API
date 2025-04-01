3de8a79e98dfac404f0edd761e6e974f8acf69a2
Blessing-09 <blessingogbogu@gmail.com>
2025-04-01 21:06:34 +0200

    Indentation adjustments

--- src/js/components/ToDo-List/Todo-list.jsx
+++ src/js/components/ToDo-List/Todo-list.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Todo-list.css"
+import "./Todo-list.css";
 
 const Todolist = () => {
   const [tasks, setTasks] = useState("");
@@ -43,21 +43,22 @@ const Todolist = () => {
   const addTask = async () => {
     let response = await fetch(
       "https://playground.4geeks.com/todo/todos/Blessing",
-      { method: "POST",
+      {
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ 
-            "label": tasks,
-            "is_done": false
-          }),
+        },
+        body: JSON.stringify({
+          label: tasks,
+          is_done: false,
+        }),
       }
     );
-    console.log(response)
+    console.log(response);
     const data = await response.json();
-    console.log(data)
-    fetchTasks ();
-    setTasks ("");
+    console.log(data);
+    fetchTasks();
+    setTasks("");
   };
 
   const deleteTask = async (todo_id) => {
@@ -67,21 +68,22 @@ const Todolist = () => {
         method: "DELETE",
       }
     );
-    console.log(response)
+    console.log(response);
     if (response.ok) {
       setTasksList((prev) => prev.filter((task) => task.id !== todo_id));
-     
     } else {
       console.log("Error deleting task");
     }
-
   };
 
   const showList = () => {
     let mapList = tasksList.map((item) => {
-      return <li className="col-12 singleList mt-1" key={item.id}>{item.label}
-      <button onClick={() => deleteTask(item.id)}>🗑</button>
-      </li>;
+      return (
+        <li className="col-12 singleList mt-1" key={item.id}>
+          {item.label}
+          <button onClick={() => deleteTask(item.id)}>🗑</button>
+        </li>
+      );
     });
     return mapList;
   };
@@ -89,12 +91,15 @@ const Todolist = () => {
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
       addTask();
-      }
-      };
+    }
+  };
 
   return (
     <div className="container mt-5 d-flex flex-column justify-content-between align-items-center">
-      <h1 className="bg-secondary px-3 py-2"><span className="totalTask mx-2">{tasksList.length}</span>MY-TO-DO-LIST <span className="totalTask">{tasksList.length > 0 ? "🔋" : "🪫"}</span></h1>
+      <h1 className="bg-secondary px-3 py-2">
+        <span className="totalTask mx-2">{tasksList.length}</span>MY-TO-DO-LIST{" "}
+        <span className="totalTask">{tasksList.length > 0 ? "🔋" : "🪫"}</span>
+      </h1>
       <div>
         <input
           type="text"
@@ -103,9 +108,11 @@ const Todolist = () => {
           onChange={(e) => setTasks(e.target.value)}
           onKeyDown={handleKeyPress}
         />
-        <button className="mx-1" onClick={addTask}>Add task</button>
+        <button className="mx-1" onClick={addTask}>
+          Add task
+        </button>
       </div>
-        <div className="container mt-3">
+      <div className="container mt-3">
         <ul className="row taskBox">{showList()}</ul>
       </div>
     </div>
